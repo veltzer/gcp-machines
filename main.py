8528420d0f1931799d33e817ccc5752e5683743a
Mark Veltzer <mark.veltzer@gmail.com>
2024-07-05 16:05:58 +0300

    no commit message given

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-from flask import Flask, render_template, redirect, url_for
-from google.appengine.api import users
+"""
+main application
+"""
+
+from flask import Flask, redirect, url_for, request, session
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
 
 app = Flask(__name__)
+app.secret_key = "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"
+CLIENT_ID = "57950378250-95roigvlmlbj1ioknv2sge4ic5s28vlo.apps.googleusercontent.com"
 
 # List of allowed users (replace with your actual user data)
 ALLOWED_USERS = {"mark.veltzer@gmail.com"}
 
-@app.route("/")
-def index():
-    user = users.get_current_user()
-    if user and user.email() in ALLOWED_USERS:
-        return render_template("welcome.html", username=user.nickname())
-    return redirect(users.create_login_url(url_for("index")))
+@app.route('/login', methods=['POST'])
+def login():
+    """ login endpoint """
+    token = request.form['credential']
+    try:
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+        # Process user information from idinfo (email, etc.)
+        session['user_id'] = idinfo['sub']  # Store user ID in session
+        return redirect(url_for('index'))  # Redirect to the main page
+    except ValueError:
+        # Invalid token
+        return "Invalid Token", 400
 
 @app.route("/logout")
 def logout():
-    return redirect(users.create_logout_url(url_for("index")))
+    """ logout endpoint """
+    if "user_id" in session:
+        del session["user_id"]
+    return redirect(url_for("/login"))
