c79d26446cda0803d9ca837e15fdd5a29546aebb
Mark Veltzer <mark.veltzer@gmail.com>
2024-07-14 20:47:47 +0300

    no commit message given

--- apis.sh
+++ apis.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 gcloud services list --filter="name:compute.googleapis.com"
 gcloud services list --filter="name:datastore.googleapis.com"
+gcloud services list --filter="name:gmail.googleapis.com"
+
 # gcloud services list --enabled
