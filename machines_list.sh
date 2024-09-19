ee9f24c18a922440f7ff3120681ed84ca47bf14c
Mark Veltzer <mark.veltzer@gmail.com>
2024-09-19 10:16:24 +0300

    no commit message given

--- machines_list.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-#!/bin/bash
-
-# Ensure the Google Cloud SDK is installed and configured
-# If not, you'll need to install and set it up first
-gcloud compute instances list \
-  --format="table(networkInterfaces[0].accessConfigs[0].natIP, labels)"
