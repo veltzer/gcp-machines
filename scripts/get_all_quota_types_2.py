f8b892b27fd1b3bdf69a77de1087c1f3f908f71a
Mark Veltzer <mark.veltzer@gmail.com>
2024-09-25 10:11:10 +0300

    linting

--- scripts/get_all_quota_types_2.py
+++ scripts/get_all_quota_types_2.py
@@ -7,15 +7,10 @@ def list_gcp_quotas():
     Lists all quota types in Google Cloud Platform (GCP).
     """
     service = build('compute', 'v1')
-    
-    try:
-        quota_types = service.projects().get(project='veltzer-machines-id').execute()['quotas']
-        
-        for quota in quota_types:
-            print(f"Quota type: {quota['metric']}")
-            
-    except Exception as e:
-        print(f"Error: {e}")
+    # pylint: disable=no-member
+    quota_types = service.projects().get(project='veltzer-machines-id').execute()['quotas']
+    for quota in quota_types:
+        print(f"Quota type: {quota['metric']}")
 
 if __name__ == "__main__":
     list_gcp_quotas()
--- scripts/get_all_quota_types_3.py
+++ scripts/get_all_quota_types_3.py
@@ -9,12 +9,14 @@ def list_gcp_quotas(project_id):
     service = build("compute", "v1")
 
     # Get project-level quotas
+    # pylint: disable=no-member
     project_quotas = service.projects().get(project=project_id).execute()["quotas"]
     print("Project-level Quotas:")
     for quota in project_quotas:
         print(f"  Quota type: {quota['metric']}")
 
     # Get regional quotas
+    # pylint: disable=no-member
     regions = service.regions().list(project=project_id).execute()["items"]
     for region in regions:
         region_name = region["name"]
