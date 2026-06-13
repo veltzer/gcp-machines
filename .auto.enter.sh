4c60c84f57581e5155c290500a2a45aee63ff74e
Mark Veltzer <mark.veltzer@gmail.com>
2026-06-13 19:28:45 +0300

    no commit message given

--- .auto.enter.sh
+++ .auto.enter.sh
@@ -1,4 +1,5 @@
 if PROJECT_ID=$(pygooglecloud get_project_id); then
 	export PROJECT_ID
 	export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/.credentials/${PROJECT_ID}.json"
+	pygooglecloud check_credentials >/dev/null
 fi
